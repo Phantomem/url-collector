f0e5b94e22318c60af09a9ddb16709577fa06e2b
Pawel Dudek <pawel@Pawels-MacBook-Pro.local>
2022-12-03 15:46:29 +0100

    added functional api tests

--- test/functional/nasaRouter.test.ts
+++ test/functional/nasaRouter.test.ts
@@ -1,30 +1,29 @@
 import request from 'supertest';
-import { Express } from 'express';
 import { application } from '../../src';
-import { getApodUrls, getRequest, getRequests } from '../../src/nasa/apodService';
+import { request as getRequest } from '../../src/lib/request';
 
-jest.mock('../../src/nasa/apodService');
+jest.mock('../../src/lib/request');
 
 describe('Nasa router test', () => {
-  let app: Express;
+  let app;
 
   beforeAll(() => {
-    app = application;
+    app = request.agent(application.listen(process.env.PORT));
   });
 
   it('Should response with status code 400', async () => {
-    const response = await request(app).get('/').send();
+    const response = await app.get('/').send();
 
     expect(response.status).toBe(400);
     expect(response.body).toMatchSnapshot();
   });
 
   it('Should response with status code 200', async () => {
-    (getApodUrls as jest.Mock).mockResolvedValueOnce([
-      "https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_3000.jpg",
-    ]);
+    (getRequest as jest.Mock).mockResolvedValueOnce([{
+      hdurl: "https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_3000.jpg",
+    }]);
 
-    const response = await request(app)
+    const response = await app
       .get('/?startDate=2019-01-01&endDate=2019-01-01')
       .send();
 
@@ -32,24 +31,39 @@ describe('Nasa router test', () => {
     expect(response.body).toMatchSnapshot();
   });
 
+  it('Should response with status code 503', async () => {
+    (getRequest as jest.Mock).mockImplementation(async () => new Promise((res) => {
+      setTimeout(() => {
+        res([{ hdurl: "https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_3000.jpg"} ])
+      }, 0)
+    }));
+
+    const response = await app
+      .get('/?startDate=2010-01-01&endDate=2019-01-01')
+      .send();
+    
+    expect(response.status).toBe(503);
+    expect(response.body).toMatchSnapshot();
+  });
 
   it('Should response with status code 503', async () => {
-    (getRequests as jest.Mock).mockImplementation(() => new Promise((res) => {
+    (getRequest as jest.Mock).mockImplementation(async () => new Promise((res) => {
       setTimeout(() => {
-        console.log('2313213');
-        res(["https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_3000.jpg"])
+        res([{ hdurl: "https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_3000.jpg"} ])
       }, 1000)
     }));
 
-    const apiRequest = request(app)
-      .get('/?startDate=2010-01-01&endDate=2019-01-01')
-      .send();
-
-    const response = await Promise.all(
-      Array.from({ length: 10 }).map(_ => apiRequest),
-    )
-    console.log(response.map(r => r.status));
+    const response = await Promise.all([
+      app.get('/?startDate=2018-01-01&endDate=2019-01-01').send(),
+      app.get('/?startDate=2017-01-01&endDate=2018-01-01').send(),
+      app.get('/?startDate=2016-01-01&endDate=2017-01-01').send(),
+      app.get('/?startDate=2015-01-01&endDate=2016-01-01').send(),
+      app.get('/?startDate=2014-01-01&endDate=2015-01-01').send(),
+      app.get('/?startDate=2013-01-01&endDate=2014-01-01').send(),
+    ]);
+    
+    expect(response).toBeDefined();
     expect(response[5].status).toBe(503);
-    // expect(response.body).toMatchSnapshot();
+    expect(response[5].body).toMatchSnapshot();
   });
 });
